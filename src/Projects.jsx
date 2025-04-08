6783a9973be67b14463493514e8b2078699ce563
dannymccarragher <dannymccarragher@gmail.com>
2025-04-08 11:38:03 -0700

    Completed Projects Page

---
+++ src/Projects.jsx
@@ -0,0 +1,29 @@
+const Projects = () => {
+
+  const projects = [
+    {
+      name: "MacroMate",
+      description:
+        "Full Stack Application to enter the food you consumed to track MacroNutrients and Calories",
+    },
+    {
+      name: "Office of The Registrar ChatBot",
+      description: "Used AI Tools to build  chatbot to handle FAQ Questions",
+    },
+  ];
+
+  const projectName = projects.map(project => <h4> {project.name} </h4>);
+  const projectDescription = projects.map(project => <p> {project.description}</p>)
+
+  return (
+    <>
+    <div>
+      <h3> Projects </h3>
+      {projectName}
+      {projectDescription}
+    </div>
+    </>
+  );
+};
+
+export default Projects;
