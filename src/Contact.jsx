eab175d45a526a0ce926ca1097f791a44ab4181a
dannymccarragher <dannymccarragher@gmail.com>
2025-04-08 11:39:56 -0700

    Begiin on Contact Page

---
+++ src/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return(
+        <>
+        <p>Phone Number: 253.359.4037</p>
+        <p>Email: dannymccarrgher@gmail.com</p>
+        </>
+    )
+}
+
+export default Contact;
